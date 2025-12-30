0e8fbe46dd66e9a1121af7a9dfe171aa22d6d407
Puligadda Shashanka Mouli <143326059+shashankamouli534@users.noreply.github.com>
2025-12-30 04:03:58 -0800

    Refactor axi_transaction class and remove redundancy

--- rtl/tb/axi_trans.sv
+++ rtl/tb/axi_trans.sv
@@ -1,28 +1,18 @@
-class axi_transaction;
+    class axi_transaction;
+        rand axi_cmd_t cmd;
+        rand logic [31:0] addr;
+        rand logic [31:0] wdata;
 
-    typedef enum logic { AXI_READ, AXI_WRITE } axi_cmd_t;
+        logic [31:0] rdata;
+        logic [1:0]  resp;
 
-    rand axi_cmd_t cmd;
-    rand logic [31:0] addr;
-    rand logic [31:0] wdata;
+        constraint aligned { addr[1:0] == 2'b00; }
 
-         logic [31:0] rdata;
-         logic [1:0]  resp;
-
-    constraint aligned { addr[1:0] == 2'b00; }
-
-    function new();
-        cmd = AXI_READ;
-        addr = 0;
-        wdata = 0;
-        rdata = 0;
-        resp = 0;
-    endfunction
-
-    function void display(string tag="");
-        $display("%0t %s CMD=%s ADDR=%h WDATA=%h RDATA=%h RESP=%0d",
-            $time, tag,
-            (cmd==AXI_WRITE)?"WRITE":"READ",
-            addr, wdata, rdata, resp);
-    endfunction
-endclass
+        function new();
+            cmd   = AXI_READ;
+            addr  = 0;
+            wdata = 0;
+            rdata = 0;
+            resp  = 0;
+        endfunction
+    endclass
